a4c4df4496e86ab97f40d4c4d52685438ea147a6
Marcelo <marcelonespoli@outlook.com>
2023-11-14 21:42:00 +0000

    Add Infra IoC

---
+++ MicroRabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbitMQ.Domain.Core.Bus;
+using MicroRabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void Register(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
